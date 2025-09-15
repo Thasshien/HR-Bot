97893501427f6151ea46e97d9e9717ccf83adf63
Thasshien <daktrboys05@gmail.com>
2025-09-16 01:34:25 +0530

    Update requirements and modify rasacontroller

--- DataSprintBackend/controllers/rasaController.js
+++ DataSprintBackend/controllers/rasaController.js
@@ -3,11 +3,11 @@ const url = "http://localhost:5005/webhooks/rest/webhook";
 
 const askReq = async (req, res) => {
     try {
-        const { query } = req.body;  // destructure the string
+        const { query } = req.body;
         console.log("query:", query);
 
         if (!query) {
-            return res.status(400).json({ "message": "No queries" });
+            return res.status(400).json({ message: "No queries" });
         }
 
         const reply = await axios.post(url, {
@@ -15,11 +15,14 @@ const askReq = async (req, res) => {
             message: query
         });
 
-        console.log(reply.data)
-        res.status(200).json({ reply: reply.data });  // send only the response data
+        // Extract texts from Rasa response
+        const texts = reply.data.map(r => r.text).join(" "); 
+
+        console.log(texts);
+        res.status(200).json({ reply: texts });  // send only string
     } catch (error) {
         console.error("askReq error:", error.message);
-        res.status(500).json({ "message": "Internal Server Error" });
+        res.status(500).json({ message: "Internal Server Error" });
     }
 };
 
